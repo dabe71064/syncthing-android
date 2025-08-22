d80c24aa49b522cd9306dd31b64a17813617c8c3
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-08-22 17:47:55 +0200

    Update AppConfigReceiver.java

--- app/src/main/java/com/nutomic/syncthingandroid/receiver/AppConfigReceiver.java
+++ app/src/main/java/com/nutomic/syncthingandroid/receiver/AppConfigReceiver.java
@@ -63,6 +63,7 @@ public class AppConfigReceiver extends BroadcastReceiver {
             case ACTION_FOLLOW:
                 Log.d(TAG, "followRunConditions by intent");
                 setPrefBtnStateForceStartStopAndNotify(context, Constants.BTNSTATE_NO_FORCE_START_STOP);
+                BootReceiver.startServiceCompat(context);
                 break;
             case ACTION_START:
                 Log.d(TAG, "forceStart by intent");
